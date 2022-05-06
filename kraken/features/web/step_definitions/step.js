789a0feb1d5b79d2a7705e6e85c8c12e04908219
Dario Fernando Rojas <df.rojasr1@uniandes.edu.co>
2022-05-06 00:22:19 -0500

    Primeros dos escenarios funcionalidad 1 Acceso a la plataforma

--- kraken/features/web/step_definitions/step.js
+++ kraken/features/web/step_definitions/step.js
@@ -1,4 +1,5 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
+const expect = require('chai').expect;
 
 Given("I have {int} cukes in my belly", function (cukes) {
   console.log(`Cukes: ${cukes}`);
@@ -17,8 +18,43 @@ When("I enter password {kraken-string}", async function (password) {
   return await element.setValue(password);
 });
 
-When("I click next", async function () {
-  let element = await this.driver.$("#ember12");
+When("I click on element having id {string}", async function (id_element) {  
+  let element = await this.driver.$(id_element);  
+  return await element.click();
+});
+
+When("I click in field having css selector {string}", async function (css_selector) {  
+  let element = await this.driver.$(css_selector);  
+  return await element.click();
+});
+
+When("I click in Sing Out",  async function () {  
+  let element = await this.driver.$('div.ember-basic-dropdown-wormhole');  
 
   return await element.click();
+});
+
+When('I enter text {string}', async function (text) {
+  try {
+    await this.driver.keys(text);
+  } catch { }
+  return;
+});
+
+When('I click coordinates X equal to {int} and Y equal to {int}', async function(x, y) {
+  return await this.driver.touchAction({ action: 'tap', x: x, y: y });
+});
+
+When('I press enter', async function() {
+  return await this.driver.pressKeyCode(66);
+});
+
+Then("Close the sesion",  async function () {
+  return;
+});
+
+Then("Show label not user with that email",  async function () {
+  let element = await this.driver.$('p[class="main-error"]');  
+  let p_text = await element.getText();
+  expect(p_text).to.contain("There is no user with that email address.");
 });
